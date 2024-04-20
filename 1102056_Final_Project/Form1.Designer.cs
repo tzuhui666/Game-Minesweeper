18be8d386118c84073c286a76c97081ce1ab3201
1102056洪慈惠 <96213059+tzuhui666@users.noreply.github.com>
2024-04-21 01:32:06 +0800

    Add files via upload

---
+++ 1102056_Final_Project/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace _1102056_Final_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.新遊戲ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.再玩一次AToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel = new System.Windows.Forms.Panel();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.新遊戲ToolStripMenuItem,
+            this.再玩一次AToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(578, 36);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 新遊戲ToolStripMenuItem
+            // 
+            this.新遊戲ToolStripMenuItem.Name = "新遊戲ToolStripMenuItem";
+            this.新遊戲ToolStripMenuItem.Size = new System.Drawing.Size(107, 27);
+            this.新遊戲ToolStripMenuItem.Text = "新遊戲(&N)";
+            this.新遊戲ToolStripMenuItem.Click += new System.EventHandler(this.新遊戲ToolStripMenuItem_Click);
+            // 
+            // 再玩一次AToolStripMenuItem
+            // 
+            this.再玩一次AToolStripMenuItem.Name = "再玩一次AToolStripMenuItem";
+            this.再玩一次AToolStripMenuItem.Size = new System.Drawing.Size(102, 27);
+            this.再玩一次AToolStripMenuItem.Text = "不玩了(&E)";
+            this.再玩一次AToolStripMenuItem.Click += new System.EventHandler(this.再玩一次AToolStripMenuItem_Click);
+            // 
+            // panel
+            // 
+            this.panel.Location = new System.Drawing.Point(0, 34);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(1000, 1000);
+            this.panel.TabIndex = 1;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "boom.jpg");
+            this.imageList1.Images.SetKeyName(1, "mark.jpg");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 594);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "踩地雷";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 新遊戲ToolStripMenuItem;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ToolStripMenuItem 再玩一次AToolStripMenuItem;
+    }
+}
+
---
+++ 1102056_Final_Project/Form1.cs
@@ -0,0 +1,427 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1102056_Final_Project
+{
+    public partial class Form1 : Form
+    {
+        Button[] btns;
+
+        int flagnumber;//計算標記數量
+        int clicknumber;//計算點擊數量
+        int onex;
+        int oney;
+        int[,] numbers;
+        bool[,] isBomb;//炸彈位置確認是不是炸彈
+        bool[,] isClicked;//確認點擊開了嗎
+        bool[,] isFlaged;//確認是否有放旗子
+        bool oneclick = true;
+        
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)//生成踩地雷按鍵
+        {
+            numbers = new int[10,10];
+            isBomb = new bool[10, 10];
+            isClicked = new bool[10, 10];
+            isFlaged = new bool[10, 10];
+            clicknumber = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    numbers[i, j] = 0;
+                    isBomb[i, j] = false;
+                    isClicked[i, j] = false;
+                    isFlaged[i, j] = false;
+                }
+            }
+            btns = new Button[10*10];//100buttons
+            flagnumber = 0;
+
+            int x = 15, y = 15, z = 30, w = z + 5;
+
+            for (int i = 0; i < btns.Length; i++)//panel可以放控制屬性的東西，用這個東西生成按紐
+            {
+                Button btn = new Button();
+
+                int row = i / 10;  
+                int column = i % 10;
+
+                btn.Width = z;  //格子寬
+                btn.Height = z;  //格子高
+                btn.Left = x + column * w; //設定橫排按紐
+                btn.Top = y + row * w;  //設定直排按紐
+                btn.Font = new Font("新細明體", 15);//設定按鈕內的文字
+                btn.Visible = true;
+                btns[i] = btn;
+                this.panel.Controls.Add(btn);//在panel加入按紐
+                btn.MouseDown += new System.Windows.Forms.MouseEventHandler(click);//按鈕全部都用同一個事件
+                btn.Name = i.ToString();
+            }
+        }
+        public void click(object sender, MouseEventArgs e)//點擊格子
+        {
+            Button button = (Button)sender;
+            int mouseplace = int.Parse(button.Name);
+            int x = mouseplace / 10;
+            int y = mouseplace % 10;
+
+            if (e.Button == MouseButtons.Right)//放旗子
+            {
+                if (isClicked[x, y] == true)
+                    return;
+
+                if (isFlaged[x, y] == false)
+                {
+                    button.BackgroundImage = imageList1.Images[1];
+                    isFlaged[x, y] = true;
+                    flagnumber++;
+                }
+                else
+                {
+                    button.BackgroundImage = null;
+                    isFlaged[x, y] = false;
+                    flagnumber--;
+                }
+
+                if (flagnumber == 10)
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        for (int j = 0; j < 10; j++)
+                        {
+                            if (isFlaged[i, j] == false)
+                                continue;
+                            if (isBomb[i, j] == false)
+                                return;
+                        }
+                    }
+                    win();
+                }
+            }
+
+            else if (e.Button == MouseButtons.Left)//開格子
+            {
+                if (oneclick)//第一次按才會擺放炸彈
+                {
+                    onex = x;
+                    oney = y;
+                    isBomb[x, y] = true;
+                    newmap();
+                    isBomb[x, y] = false;
+                    oneclick = false;
+                }
+
+                if (isClicked[x, y])
+                    return;
+
+                clicknumber++;
+
+                if (isBomb[x, y])
+                {
+                    gameover();
+                    return;
+                }
+                isClicked[x, y] = true;
+
+                button.BackColor = Color.DarkGray;
+
+                if (numbers[x, y] != 0)
+                    button.Text = numbers[x, y].ToString();
+
+                map(x, y);
+
+                if (clicknumber == 10 * 10 - 10)
+                    win();
+            }
+
+        }
+        public void newmap()//生成10*10的大地圖，並將炸彈隨機擺放
+        {
+            Random r = new Random();
+            int x, y;
+            for (int i = 0; i < 10; i++)
+            {
+                do
+                {
+                    x = r.Next(10);
+                    y = r.Next(10);
+                }
+                while (isBomb[x, y] == true);
+                    isBomb[x, y] = true;
+                //以炸彈為中心的八個方位，確認炸彈周圍格子的數字
+                if (x != 0 && y != 0)
+                    numbers[x - 1, y - 1]++;
+                if (x != 0)
+                    numbers[x - 1, y]++;
+                if (x != 0 && y != 10 - 1)
+                    numbers[x - 1, y + 1]++;
+                if (x != 10 - 1 && y != 0)
+                    numbers[x + 1, y - 1]++;
+                if (x != 10 - 1)
+                    numbers[x + 1, y]++;
+                if (x != 10 - 1 && y != 10 - 1)
+                    numbers[x + 1, y + 1]++;
+                if (y != 0)
+                    numbers[x, y - 1]++;
+                if (y != 10 - 1)
+                    numbers[x, y + 1]++;
+            }
+
+        }
+        public void map(int x, int y)//以點擊為中心的八個方位，判斷是否為數字，不是數字的話就擴散出去，直到碰到數字
+        {
+            if (numbers[x, y] > 0)//有大於0的數字就顯示
+                btns[x * 10 + y].Text = numbers[x, y].ToString();
+            clicknumber++;
+            if (numbers[x, y] == 0)//有等於0的數字表示空白要擴張
+            {
+                if (x != 0 && y != 0 && isBomb[x - 1, y - 1] == false && isClicked[x - 1, y - 1] == false)//先判斷所選格子是否為100方格子中的邊界
+                {
+                    isClicked[x - 1, y - 1] = true;
+                    btns[(x - 1) * 10 + y - 1].BackColor = Color.DarkGray;
+                    clicknumber++;//點擊次數增加
+                    if (isFlaged[x - 1, y - 1] == true)
+                    {
+                        isFlaged[x - 1, y - 1] = false;
+                        btns[(x - 1) * 10 + y - 1].BackgroundImage = null;//刪掉該格旗子
+                    }
+                    if (numbers[x - 1, y - 1] != 0)
+                        btns[(x - 1) * 10 + y - 1].Text = numbers[x - 1, y - 1].ToString();//顯示數字
+                    else
+                    {
+                        map(x - 1, y - 1);//空白的繼續擴張空白
+                    }
+                }
+                if (x != 0 && isBomb[x - 1, y] == false && isClicked[x - 1, y] == false)
+                {
+                    isClicked[x - 1, y] = true;
+                    btns[(x - 1) * 10 + y].BackColor = Color.DarkGray;
+                    clicknumber++;
+
+                    if (isFlaged[x - 1, y] == true)
+                    {
+                        isFlaged[x - 1, y - 1] = false;
+                        btns[(x - 1) * 10 + y].BackgroundImage = null;
+                    }
+
+                    if (numbers[x - 1, y] != 0)
+                        btns[(x - 1) * 10 + y].Text = numbers[x - 1, y].ToString();
+                    else
+                    {
+                        map(x - 1, y);
+                    }
+                }
+                if (x != 0 && y != 10 - 1 && isBomb[x - 1, y + 1] == false && isClicked[x - 1, y + 1] == false)
+                {
+                    isClicked[x - 1, y + 1] = true;
+                    btns[(x - 1) * 10 + y + 1].BackColor = Color.DarkGray;
+                    clicknumber++;
+
+                    if (isFlaged[x - 1, y + 1] == true)
+                    {
+                        isFlaged[x - 1, y + 1] = false;
+                        btns[(x - 1) * 10 + y + 1].BackgroundImage = null;
+                    }
+
+                    if (numbers[x - 1, y + 1] != 0)
+                        btns[(x - 1) * 10 + y + 1].Text = numbers[x - 1, y + 1].ToString();
+                    else
+                    {
+                        map(x - 1, y + 1);
+                    }
+                }
+                if (y != 0 && isBomb[x, y - 1] == false && isClicked[x, y - 1] == false)
+                {
+                    isClicked[x, y - 1] = true;
+                    btns[x * 10 + y - 1].BackColor = Color.DarkGray;
+
+                    if (isFlaged[x, y - 1] == true)
+                    {
+                        isFlaged[x, y - 1] = false;
+                        btns[x * 10 + y - 1].BackgroundImage = null;
+                    }
+
+                    if (numbers[x, y - 1] != 0)
+                        btns[x * 10 + y - 1].Text = numbers[x, y - 1].ToString();
+                    else
+                    {
+                        map(x, y - 1);
+                    }
+                }
+                if (y != 10 - 1 && isBomb[x, y + 1] == false && isClicked[x, y + 1] == false)
+                {
+                    isClicked[x, y + 1] = true;
+                    btns[x * 10 + y + 1].BackColor = Color.DarkGray;
+
+                    if (isFlaged[x, y + 1] == true)
+                    {
+                        isFlaged[x, y + 1] = false;
+                        btns[x * 10 + y + 1].BackgroundImage = null;
+                    }
+
+                    if (numbers[x, y + 1] != 0)
+                        btns[x * 10 + y + 1].Text = numbers[x, y + 1].ToString();
+                    else
+                    {
+                        map(x, y + 1);
+                    }
+                }
+                if (x != 10 - 1 && y != 0 && isBomb[x + 1, y - 1] == false && isClicked[x + 1, y - 1] == false)
+                {
+                    isClicked[x + 1, y - 1] = true;
+                    btns[(x + 1) * 10 + y - 1].BackColor = Color.DarkGray;
+
+                    if (isFlaged[x + 1, y - 1] == true)
+                    {
+                        isFlaged[x + 1, y - 1] = false;
+                        btns[(x + 1) * 10 + y - 1].BackgroundImage = null;
+                    }
+
+                    if (numbers[x + 1, y - 1] != 0)
+                        btns[(x + 1) * 10 + y - 1].Text = numbers[x + 1, y - 1].ToString();
+                    else
+                    {
+                        map(x + 1, y - 1);
+                    }
+                }
+                if (x != 10 - 1 && isBomb[x + 1, y] == false && isClicked[x + 1, y] == false)
+                {
+                    isClicked[x + 1, y] = true;
+                    btns[(x + 1) * 10 + y].BackColor = Color.DarkGray;
+
+                    if (isFlaged[x + 1, y] == true)
+                    {
+                        isFlaged[x + 1, y] = false;
+                        btns[(x + 1) * 10 + y].BackgroundImage = null;
+                    }
+
+                    if (numbers[x + 1, y] != 0)
+                        btns[(x + 1) * 10 + y].Text = numbers[x + 1, y].ToString();
+                    else
+                    {
+                        map(x + 1, y);
+                    }
+                }
+                if (x != 10 - 1 && y != 10 - 1 && isBomb[x + 1, y + 1] == false && isClicked[x + 1, y + 1] == false)
+                {
+                    isClicked[x + 1, y + 1] = true;
+                    btns[(x + 1) * 10 + y + 1].BackColor = Color.DarkGray;
+
+                    if (isFlaged[x + 1, y + 1] == true)
+                    {
+                        isFlaged[x + 1, y + 1] = false;
+                        btns[(x + 1) * 10 + y + 1].BackgroundImage = null;
+                    }
+
+                    if (numbers[x + 1, y + 1] != 0)
+                        btns[(x + 1) * 10 + y + 1].Text = numbers[x + 1, y + 1].ToString();
+                    else
+                    {
+                        map(x + 1, y + 1);
+                    }
+                }
+                return;
+            }
+        }
+
+        public void win()//遊戲贏後要把炸彈給他們看，然後除了炸彈其他位置要顯示灰格子跟數字
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (isFlaged[i, j] == true)
+                    {
+                        isFlaged[i, j] = false;
+                        btns[i * 10 + j].BackgroundImage = null;
+                    }
+
+                    if (isClicked[i, j] == true)
+                        continue;
+
+                    btns[i * 10 + j].BackColor = Color.DarkGray;
+
+                    isClicked[i, j] = true;
+
+                    if (isBomb[i, j])
+                    {
+                        btns[i * 10 + j].BackgroundImage = imageList1.Images[0];
+                        continue;
+                    }
+
+                    if (numbers[i, j] != 0)
+                        btns[i * 10 + j].Text = numbers[i, j].ToString();
+                }
+            }
+            MessageBox.Show("你贏了");
+            for (int i = 0; i < 10; i++)
+                for (int j = 0; j < 10; j++)
+                    btns[i].Enabled = false;
+        }
+        public void gameover()//遊戲輸後要把炸彈給他們看，然後除了炸彈其他位置要顯示灰格子跟數字
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (isFlaged[i, j] == true)
+                    {
+                        isFlaged[i, j] = false;
+                        btns[i * 10 + j].BackgroundImage = null;
+                    }
+
+                    if (isClicked[i, j] == true)
+                        continue;
+
+                    btns[i * 10 + j].BackColor = Color.DarkGray;
+
+                    isClicked[i, j] = true;
+
+                    if (isBomb[i, j])
+                    {
+                        btns[i * 10 + j].BackgroundImage = imageList1.Images[0];
+                        continue;
+                    }
+                    if (numbers[i, j] != 0)
+                        btns[i * 10 + j].Text = numbers[i, j].ToString();
+
+                }
+            }
+
+            MessageBox.Show("你輸了！");
+            for (int i = 0; i < 10; i++)
+                for (int j = 0; j < 10; j++)
+                    btns[i].Enabled = false;
+        }
+
+
+        
+
+       
+        
+        private void 新遊戲ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void 再玩一次AToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
